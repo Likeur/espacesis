085794c3935a2834f387f400ae3ffedecd88604a
Likeur <jacksimba28@gmail.com>
2023-12-20 01:44:05 +0200

    adding slider component

--- src/app/components/herosec/herosec.component.ts
+++ src/app/components/herosec/herosec.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+// import function to register Swiper custom elements
+// import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+//register();
 
 @Component({
   selector: 'app-herosec',
   standalone: true,
   imports: [],
   templateUrl: './herosec.component.html',
-  styles: ``
+  // schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HerosecComponent {
-
-}
+export class HerosecComponent {}
