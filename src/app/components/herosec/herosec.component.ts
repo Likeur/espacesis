7254d6f5edd35c3d1ed1a1dac78c401d5a68682f
Likeur <jacksimba28@gmail.com>
2023-12-20 13:36:29 +0200

    adding metric and partner section

--- src/app/components/herosec/herosec.component.ts
+++ src/app/components/herosec/herosec.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
+import { MetricComponent } from '../metric/metric.component';
 
 
 @Component({
   selector: 'app-herosec',
   standalone: true,
-  imports: [SliderComponent],
+  imports: [SliderComponent, MetricComponent],
   templateUrl: './herosec.component.html'
 })
 export class HerosecComponent {}
---
+++ src/app/components/metric/metric.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  standalone: true,
+  imports: [],
+  templateUrl: './metric.component.html',
+  styles: ``
+})
+export class MetricComponent {
+
+}
