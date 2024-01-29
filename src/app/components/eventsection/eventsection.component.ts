895d0c3c5338a64ab337161addf38cb2a3674ff2
Likeur <jacksimba28@gmail.com>
2024-01-29 03:46:03 +0200

    working on cta

--- src/app/components/eventsection/eventsection.component.ts
+++ src/app/components/eventsection/eventsection.component.ts
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class EventsectionComponent {
 
+  eventList = [
+    {
+      id:1,
+      name : 'Devfest SE2023',
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      albumLink : ""
+    },
+    {
+      id:2,
+      name : 'Soft Day',
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      albumLink : ""
+    },
+    {
+      id:3,
+      name : 'Code Fiesta Ed2',
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      albumLink : ""
+    },
+  ]
 }
--- src/app/components/teamsection/teamsection.component.ts
+++ src/app/components/teamsection/teamsection.component.ts
@@ -15,56 +15,64 @@ export class TeamsectionComponent {
       photoLink : '../../../assets/images/like.jpg',
       name:'Chrinovic MM',
       post: 'Lead',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:2,
       photoLink : '../../../assets/images/like.jpg',
       name:'John Muska',
       post: 'co-lead',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:3,
       photoLink : '../../../assets/images/like.jpg',
       name:'Christian',
       post: 'co-lead',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:4,
       photoLink : '../../../assets/images/like.jpg',
       name:'Likeur off',
       post: 'Designer',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:5,
       photoLink : '../../../assets/images/like.jpg',
       name:'Athia',
       post: 'Tresorière',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:6,
       photoLink : '../../../assets/images/like.jpg',
       name:'Altha',
       post: 'MC & Sec',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:7,
       photoLink : '../../../assets/images/like.jpg',
       name:'Glodie Lukose',
       post: 'Web',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     },
     {
       id:8,
       photoLink : '../../../assets/images/like.jpg',
       name:'L kanan',
       post: 'ML',
-      linkedin: '...'
+      linkedin: '...',
+      instagram : '...'
     }
   ]
 } 
