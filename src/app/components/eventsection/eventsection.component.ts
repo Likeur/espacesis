182efee61196f094bb9327b693a26b0a531e765a
Likeur <jacksimba28@gmail.com>
2024-01-30 03:34:56 +0200

    adding event links

--- src/app/components/eventsection/eventsection.component.ts
+++ src/app/components/eventsection/eventsection.component.ts
@@ -12,21 +12,24 @@ export class EventsectionComponent {
   eventList = [
     {
       id:1,
+      linkphoto: '../../../assets/images/devfest2.jpg',
       name : 'Devfest SE2023',
       description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
-      albumLink : ""
+      albumLink : "https://photos.app.goo.gl/n4dLuFcuynJyKRkp6"
     },
     {
       id:2,
-      name : 'Soft Day',
+      linkphoto: '../../../assets/images/welcomeday.JPG',
+      name : 'welcom Day',
       description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
       albumLink : ""
     },
     {
       id:3,
+      linkphoto: '../../../assets/images/codefiesta.jpg',
       name : 'Code Fiesta Ed2',
       description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
-      albumLink : ""
+      albumLink : "https://photos.app.goo.gl/tns9LhFvjnb9CjGy8"
     },
   ]
 }
