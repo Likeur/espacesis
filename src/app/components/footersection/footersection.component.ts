f2c1bd0a74ce812738c1b7c2a2730fde68bda1ad
Likeur <jacksimba28@gmail.com>
2024-02-03 16:16:05 +0200

    fixe hover on cta

--- src/app/components/footersection/footersection.component.ts
+++ src/app/components/footersection/footersection.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footersection',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footersection.component.html',
-  styles: ``
 })
 export class FootersectionComponent {
 
