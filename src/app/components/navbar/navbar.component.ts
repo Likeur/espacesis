e550156c2ec27b127185a387f2400567b7c32f29
Likeur <jacksimba28@gmail.com>
2024-02-05 15:51:42 +0200

    working on responsive navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import {gsap} from 'gsap';
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  
+
+  openMenuTl = gsap.timeline({
+    defaults:{
+      duration:1,
+      ease:'power4.inOut'
+    }
+  })
+
+  ngOnInit(): void {
+    gsap.set('.nav_Wrapper',{
+      scaleY:0
+    })
+    gsap.set('.nav_links',{
+      y:100
+    })
+    gsap.set('.nav_container',{
+      display:'none'
+    })  
+    
+  }
+  
+  menuIsOpen = false
+  
+  toggleMenu(){
+
+    this.openMenuTl.to('.nav_container',{
+      display:'grid'
+    }).to('.nav_Wrapper',{
+      scaleY:1,
+      transformOrigin:'top'
+    }, "<").to('.btn_barres',{
+      backgroundColor:'white'
+    }, "<").to('.nav_links',{
+      y:0,
+      stagger:0.1,
+    }, "<")
+
+  this.openMenuTl.pause()
+
+    if(!this.menuIsOpen){
+      this.openMenuTl.play()
+      this.menuIsOpen = true
+    }else{
+      this.openMenuTl.reverse()
+      this.menuIsOpen = false
+    }
+  }
+}
