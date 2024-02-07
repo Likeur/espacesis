fd5cbbde0cd07ab5728fb9c5fc2bdd93827b7eee
Likeur <jacksimba28@gmail.com>
2024-02-07 02:57:35 +0200

    adding text on event section

--- src/app/components/eventsection/eventsection.component.ts
+++ src/app/components/eventsection/eventsection.component.ts
@@ -14,21 +14,21 @@ export class EventsectionComponent {
       id:1,
       linkphoto: '../../../assets/images/devfest2.jpg',
       name : 'Devfest SE2023',
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      description: "Les DevFests sont des conférences locales organisées par les groupes de développeurs Google (GDG) dans le monde entier. Chaque DevFest est conçu par ses organisateurs locaux pour répondre aux besoins et aux intérêts de la communauté de développeurs locale.",
       albumLink : "https://photos.app.goo.gl/n4dLuFcuynJyKRkp6"
     },
     {
       id:2,
       linkphoto: '../../../assets/images/welcomeday.JPG',
-      name : 'welcom Day',
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      name : 'welcome Day',
+      description: "Est une journée pour acceuillir les nouveau membres du club et leur présenter l'objectif du club , le bénéfice, la vision , la présentation des projets par les anciens membres et quelques retours d'expériences de la part des alumnis du club pour encourager les nouveaux membres.",
       albumLink : ""
     },
     {
       id:3,
       linkphoto: '../../../assets/images/codefiesta.jpg',
       name : 'Code Fiesta Ed2',
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia facere eius dolor officiis laboriosam a ut nihil odit ullam nisi nemo ad molestias nesciunt, fuga accusantium deserunt hic amet nulla!",
+      description: "Code fiesta est un évennement dans lesquels les développeurs de la communauté se rétrouve pour faire differentes activités comme des challenges de code , des live coding , festoyer et quelques retours d'experiences.",
       albumLink : "https://photos.app.goo.gl/tns9LhFvjnb9CjGy8"
     },
   ]
